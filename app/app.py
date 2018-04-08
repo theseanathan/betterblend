394c0bf04b3f69c048ac6ea7977f2b4e866a1e6f
Sean Darryanto <sdarryanto@lyft.com>
2018-04-07 18:05:12 -0700

    Refactor

--- app/app.py
+++ app/app.py
@@ -1,22 +1,21 @@
 import client_info
 
-from flask import Flask, Blueprint, request, redirect, url_for
+from flask import Flask, Blueprint, request, redirect
 import base64
 import random
 import requests
 import string
 import urllib
+import json
 
 app = Flask(__name__)
 blueprint = Blueprint('spotify_api', __name__)
 
+api_url_base = 'https://api.spotify.com/v1/{endpoint}'
 redirect_uri = 'http://localhost:5000/callback'
 
 
-@blueprint.route('/', methods=['GET'])
-@blueprint.route('/login', methods=['GET'])
 def login():
-    print("WHAT THE FUCK")
     url = 'https://accounts.spotify.com/authorize?'
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
     scope = 'user-read-private user-read-email'
@@ -27,9 +26,9 @@ def login():
         'state': state,
         'scope': scope,
     }
-    print("SENDING GET")
     # response = requests.get(url, params=auth_dict)
-    return redirect(url + urllib.urlencode(auth_dict))
+    redirect_str = url + urllib.urlencode(auth_dict)
+    return redirect(redirect_str)
 
 
 @blueprint.route('/callback', methods=['GET'])
@@ -39,31 +38,32 @@ def callback():
     state = request.args.get('state')
     if state is not None:
         b64_client = base64.b64encode('{}:{}'.format(client_info.CLIENT_ID, client_info.CLIENT_SECRET))
-        print("ENCODED:        ", b64_client)
-        print("DECODED:        ", base64.b64decode(b64_client))
-        print("AUTHORIZATION:   Basic: {}".format(b64_client))
         token_header = {
-            'Authorization': 'Basic {}'.format(b64_client),
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Authorization': 'Basic {}'.format(b64_client)
         }
-        # 'client_id': client_info.CLIENT_ID,
-        # 'client_secret': client_info.CLIENT_SECRET,
         auth_dict = {
-            'code': code,
+            'code': code.encode('ascii'),
             'redirect_uri': redirect_uri,
             'grant_type': 'authorization_code',
         }
-        # headers = base64.b64encode('{}:{}'.format(client_info.CLIENT_ID, client_info.CLIENT_SECRET))
         response = requests.post(url, data=auth_dict, headers=token_header, json=True)
-        if response.status_code == 200:
-            print response.request.body
-            access_token = response.content
-            me_url = 'https://api.spotify.com/v1/me'
-            me_headers = {'Authorization': 'Bearer {}'.format(access_token)}
-            response = requests.get(me_url, headers=me_headers, json=True)
-            if response.status_code == 200:
-                return redirect('/#')
 
-    return response.text
+        if response.status_code == 200:
+            return response
+        else:
+            raise Exception(response)
+
+def get_playlists(auth_response):
+    response_content = json.loads(auth_response.content)
+
+    access_token = response_content.get('access_token')
+    refresh_token = response_content.get('refresh_token')
+
+    get_playlist_endpoint = 'me/playlists'
+    me_headers = {'Authorization': 'Bearer {}'.format(access_token)}
+    response = requests.get(api_url_base.format(endpoint=get_playlist_endpoint),
+                            headers=me_headers,
+                            json=True)
+    return json.loads(response.text)
 
 app.register_blueprint(blueprint)
