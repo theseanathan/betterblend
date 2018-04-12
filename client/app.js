a0b317ccd285c5f2fcad30c2421c7df406562ce6
Sean Darryanto <sdarryanto@lyft.com>
2018-04-11 18:28:51 -0700

    Auth flow - First step
    Redirects to authorization url upon visit, then goes to callback.
    Currently working on callback url to get access_token.

--- client/app.js
+++ client/app.js
@@ -1,16 +1,34 @@
 //app.js 
-const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
+const client_info = require('./client_info.json');
+const express = require('express');
+const querystring = require('querystring');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('public'));
 
 app.set('view engine', 'hbs');
 
+
 app.get('/', (req, res) => {
-	res.render('index');
+    var callback_uri = 'http://localhost:8080/callback';
+    var scope = 'user-read-private user-read-email';
+    var url = 'https://accounts.spotify.com/authorize?';
+
+    var auth_dict = {
+        response_type: 'code',
+        client_id: client_info.client_id,
+        scope: scope,
+        redirect_uri: callback_uri
+    };
+
+    res.redirect(url + querystring.stringify(auth_dict));
+});
+
+app.get('/callback', (req, res) => {
+    var code = req.query.code;
 });
 
 app.get('/playlists', (req, res) => {
