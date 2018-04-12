56603f404e39129ffc29849942d1d4eacbf77749
Sean Darryanto <sdarryanto@lyft.com>
2018-04-11 19:05:15 -0700

    Token authorization
    Needs routing to home page and saving tokens

--- client/app.js
+++ client/app.js
@@ -1,10 +1,14 @@
 //app.js 
-const app = express();
-
+const atob = require('atob')
 const bodyParser = require('body-parser');
+const btoa = require('btoa')
 const client_info = require('./client_info.json');
 const express = require('express');
 const querystring = require('querystring');
+const request = require('request')
+
+const app = express();
+const callback_uri = 'http://localhost:8080/callback';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -13,7 +17,6 @@ app.set('view engine', 'hbs');
 
 
 app.get('/', (req, res) => {
-    var callback_uri = 'http://localhost:8080/callback';
     var scope = 'user-read-private user-read-email';
     var url = 'https://accounts.spotify.com/authorize?';
 
@@ -29,6 +32,34 @@ app.get('/', (req, res) => {
 
 app.get('/callback', (req, res) => {
     var code = req.query.code;
+    var url = 'https://accounts.spotify.com/api/token';
+
+    var token_dict = {
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: callback_uri
+    };
+
+    var client_info_encrypted_btoa = btoa(client_info.client_id + ':' + client_info.client_secret)
+    var headers = { 'Authorization': 'Basic ' + client_info_encrypted_btoa };
+
+    var auth_dict = {
+        url: url,
+        form: token_dict,
+        headers: headers,
+        json: true,
+    };
+
+    request.post(auth_dict, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('access_token: ' + body.access_token);
+            console.log('refresh_token: ' + body.refresh_token);
+            // TODO: Save tokens to HTML5 Web Storage, route to HOME page.
+        } else {
+            console.log("RESPONSE: " + JSON.stringify(response))
+            console.log("BODY: " + JSON.stringify(body))
+        }
+    });
 });
 
 app.get('/playlists', (req, res) => {
