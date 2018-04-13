abc51ce9653adc1fd4c756a24f547520ca806df5
Sean Darryanto <sdarryanto@lyft.com>
2018-04-13 11:20:50 -0700

    Frontend changes to playlist
    Gets from Flask server

--- client/public/js/playlist.js
+++ client/public/js/playlist.js
@@ -6,17 +6,19 @@ function main() {
 
 function getPlaylists() {
 	let req = new XMLHttpRequest();
-	let urls = 'http://localhost:3000/response';
+    // let urls = 'http://localhost:3000/response';
+	let urls = 'http://localhost:5000/get_playlists?access_token=' + localStorage.getItem('access_token');
 	req.open('GET', urls);
-	req.addEventListener('load', function(evt) {
+
+	req.onreadystatechange = function() {
 		let t = document.querySelector('#playlist-list')
 		if(req.status >= 200 && req.status < 300) {
-			const lists = JSON.parse(req.responseText);
+            const lists = JSON.parse(req.responseText);
 			for(const p of lists.playlists) {
 				appendTable(t, p);
 			}
 		}
-	});
+	};
 	req.send();
 }
 
