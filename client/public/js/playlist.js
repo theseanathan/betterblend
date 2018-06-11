52ef738c0eb23e09cb30faf856b763d6c7579ca6
Bo Peng <pb0928@gmail.com>
2018-06-10 20:06:18 -0400

    temporary layout for playlist and tracks

--- client/public/js/playlist.js
+++ client/public/js/playlist.js
@@ -6,7 +6,7 @@ function main() {
 
 function getPlaylists() {
 	let req = new XMLHttpRequest();
-    // let urls = 'http://localhost:3000/response';
+    //let urls = 'http://localhost:3000/response';
 	let urls = 'http://localhost:5000/get_playlists?access_token=' + localStorage.getItem('access_token');
 	req.open('GET', urls);
 	//req.onreadystatechange = function() {
@@ -24,13 +24,55 @@ function getPlaylists() {
 }
 
 function appendToDiv(t, p) {
-	const tr = document.createElement('a');
-	tr.href = '/tracks';
-	tr.classList.add('playlist-item');
-	//tr.classList.add('shadow');
-	tr.classList.add('scaling');
-    //const n = document.createElement('a');
-    tr.appendChild(document.createTextNode(p.name));
-    //tr.appendChild(n);
-    t.appendChild(tr);
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	const cardImage = document.createElement('div');
+	cardImage.classList.add('card-image');
+	const figure = document.createElement('figure');
+	figure.classList.add('image');
+	figure.classList.add('is-square');
+	const img = document.createElement('img');
+	img.src = "https://bulma.io/images/placeholders/480x480.png"; //image here
+	figure.appendChild(img);
+	cardImage.appendChild(figure);
+	card.appendChild(cardImage);
+
+	const cardContent = document.createElement('div');
+	cardContent.classList.add('card-content');
+	const media = document.createElement('div');
+	media.classList.add('media');
+	const mediaContent = document.createElement('div');
+	mediaContent.classList.add('media-content');
+	const playlist = document.createElement('p');
+	playlist.classList.add('title');
+	playlist.classList.add('is-6');
+	playlist.style.textAlign = 'center';
+	const link = document.createElement('a');
+	link.appendChild(document.createTextNode(p.name)); //playlist name here
+	link.href = "/tracks";                             //href link here
+	playlist.appendChild(link);
+	mediaContent.appendChild(playlist);
+	media.appendChild(mediaContent);
+	cardContent.appendChild(media);
+	card.appendChild(cardContent);
+
+	t.appendChild(card);
 }
+
+/*
+<div class="card">
+	<div class="card-image">
+		<figure class="image is-square">
+			<img src="https://bulma.io/images/placeholders/480x480.png" alt="Placeholder image">
+		</figure>
+	</div>
+	<div class="card-content">
+		<div class="media">
+			<div class="media-content">
+				<p class="title is-6" style="text-align:center;"><a href="/tracks">Playlist</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+*/
--- client/public/js/tracks.js
+++ client/public/js/tracks.js
@@ -6,6 +6,7 @@ function main() {
 
 function getTracks() {
 	let req = new XMLHttpRequest();
+	//let urls = 'http://localhost:3000/response';
 	let urls = 'http://localhost:5000/get_tracks?access_token=' + localStorage.getItem('access_token');
 	req.open('GET', urls);
 	req.addEventListener('load', function(evt) {
@@ -22,9 +23,105 @@ function getTracks() {
 }
 
 function appendToDiv(t, p) {
-    const tr = document.createElement('div');
-    tr.href = '/tracks';
-    tr.classList.add('playlist-item');
-    tr.appendChild(document.createTextNode(p.name));
-    t.appendChild(tr);
+	const box = document.createElement('div');
+	box.classList.add('box');
+	const media = document.createElement('div');
+	media.classList.add('media');
+	
+	const mediaLeft = document.createElement('div');
+	mediaLeft.classList.add('media-left');
+	const figure = document.createElement('figure');
+	figure.classList.add('image');
+	figure.classList.add('is-64x64');
+	const img = document.createElement('img');
+	img.src = "https://bulma.io/images/placeholders/128x128.png"; //image here
+	figure.appendChild(img);
+	mediaLeft.appendChild(figure);
+	media.appendChild(mediaLeft);
+
+	const mediaContent = document.createElement('div');
+	mediaContent.classList.add('media-content');
+	const content = document.createElement('div');
+	content.classList.add('content');
+	const p1 = document.createElement('p');
+	const p2 = document.createElement('p');
+	p1.classList.add('names');
+	p1.classList.add('song-name');
+	p2.classList.add('names');
+	p2.classList.add('artist-name');
+	p1.appendChild(document.createTextNode(p.name));
+	p2.appendChild(document.createTextNode(p.artist));
+	content.appendChild(p1);
+	content.appendChild(document.createElement('br'));
+	content.appendChild(p2);
+	mediaContent.appendChild(content);
+
+	const nav = document.createElement('nav');
+	nav.classList.add('level');
+	nav.classList.add('is-mobile');
+	const left = document.createElement('div');
+	left.classList.add('level-left');
+	const a1 = document.createElement('a');
+	a1.classList.add('level-item');
+	const span1 = document.createElement('span');
+	span1.classList.add('icon');
+	span1.classList.add('is-small');
+	const i1 = document.createElement('i');
+	i1.classList.add('fas');
+	i1.classList.add('fa-chevron-circle-up');
+	span1.appendChild(i1);
+	a1.appendChild(span1);
+	const a2 = document.createElement('a');
+	a2.classList.add('level-item');
+	const span2 = document.createElement('span');
+	span2.classList.add('icon');
+	span2.classList.add('is-small');
+	const i2 = document.createElement('i');
+	i2.classList.add('fas');
+	i2.classList.add('fa-chevron-circle-up');
+	span2.appendChild(i2);
+	a2.appendChild(span2);
+	left.appendChild(a1);
+	left.appendChild(a2);
+	const right = document.createElement('div');
+	right.classList.add('level-right');
+	right.appendChild(document.createTextNode(p.vote_count));
+	nav.appendChild(left);
+	nav.appendChild(right);
+
+	mediaContent.appendChild(nav);
+	media.appendChild(mediaContent);
+	box.appendChild(media);
+	t.appendChild(box);
 }
+
+/*<div class="box">
+	<div class="media">
+		<div class="media-left">
+			<figure class="image is-64x64">
+				<img src="https://bulma.io/images/placeholders/128x128.png">
+			</figure>
+		</div>
+		<div class="media-content">
+			<div class="content">
+				<p class="names"><strong>Song NameSong NameSong NameSong NameSong NameSong NameSong Name</strong></p><br>
+				<p class="names"><small>ArtistArtistArtistArtistArtistArtistArtistArtist</small></p>
+			</div>
+			<nav class="level is-mobile">
+				<div class="level-left">
+					<a class="level-item" aria-label="up">
+						<span class="icon is-small">
+							<i class="fas fa-chevron-circle-up"></i>
+						</span>
+					</a>
+					<a class="level-item" aria-label="down">
+						<span class="icon is-small">
+							<i class="fas fa-chevron-circle-down"></i>
+						</span>
+					</a>
+				</div>
+				<div class="level-right">69</div>
+			</nav>
+		</div>
+	</div>
+</div>*/
